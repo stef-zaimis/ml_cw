23d103f50bcf47c373aa4930a13234060208e125
stefanos-zaimis <rd22097@bristol.ac.uk>
2024-11-14 18:44:29 +0000

    CT 7: Log Reg

--- scripts/classification.py
+++ scripts/classification.py
@@ -2,9 +2,9 @@ import numpy as np
 
 from sklearn.datasets import fetch_covtype
 from sklearn.model_selection import train_test_split
-from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
-from sklearn.mixture import GaussianMixture
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 # Import covtype
 X, y = fetch_covtype(return_X_y=True, as_frame=True)
@@ -12,5 +12,18 @@ X, y = fetch_covtype(return_X_y=True, as_frame=True)
 # Code Task 6
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-print(f"Training set size: {X_train.shape[0]} samples")
-print(f"Test set size: {X_test.shape[0]} samples")
+#print(f"Training set size: {X_train.shape[0]} samples")
+#print(f"Test set size: {X_test.shape[0]} samples")
+
+# Code Task 7
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.fit_transform(X_test)
+
+log_reg = LogisticRegression(max_iter=1000, solver='lbfgs', random_state=42)
+log_reg.fit(X_train_scaled, y_train)
+
+y_pred = log_reg.predict(X_test_scaled)
+accuracy = accuracy_score(y_test, y_pred)
+
+print(f"Log Reg Test Set Accurracy: {accuracy:}")
