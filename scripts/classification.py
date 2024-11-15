9eccf4d4ba18966a39d991a71bf19997a7179a34
stefanos-zaimis <rd22097@bristol.ac.uk>
2024-11-15 19:19:53 +0000

    Random Forest Ensemble

--- scripts/classification.py
+++ scripts/classification.py
@@ -31,9 +31,43 @@ accuracy_logreg = accuracy_score(y_test, y_pred_logreg)
 
 # Code Task 8
 tree_clf = DecisionTreeClassifier(random_state=42)
-tree_clf.fit(X_train_scaled, y_train)
+tree_clf.fit(X_train, y_train)
 
-y_pred_tree = tree_clf.predict(X_test_scaled)
+y_pred_tree = tree_clf.predict(X_test)
 accuracy_tree = accuracy_score(y_test, y_pred_tree)
 
 print(f"Tree Test Set Accuracy: {accuracy_tree}")
+
+# Code Task 9
+# I will implement both bagging and boosting for now to see which one performs better and which one is more suitable (probaby random forest)
+
+# RANDOM FOREST
+num_models = 20
+sample_size = 2000
+feature_sample_size = None
+np.random.seed(0)
+
+all_rf_models = []
+for m in range(num_models):
+    sample_idx = np.random.choice(X_train.shape[0], sample_size)
+    X_train_sample, y_train_sample = X_train.iloc[sample_idx], y_train.iloc[sample_idx]
+
+    rf_model = DecisionTreeClassifier(max_features=feature_sample_size)
+    rf_model.fit(X_train_sample, y_train_sample)
+
+    all_rf_models.append(rf_model)
+
+def random_forest_predict(X_test, models):
+    votes = np.zeros((X_test.shape[0], len(models)))
+    
+    for idx, model in enumerate(models):
+        votes[:, idx] = model.predict(X_test)
+    
+    predictions = np.apply_along_axis(lambda x: np.bincount(x.astype(int)).argmax(), axis=1, arr=votes)
+    
+    return predictions
+
+rf_predictions = random_forest_predict(X_test, all_rf_models)
+rf_accuracy = accuracy_score(y_test, rf_predictions)
+
+print(f"Bagging Test Set Accuracy: {rf_accuracy}")
