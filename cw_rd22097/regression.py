39276e55fc01f6620457035ef63711d3a57a7588
stefanos-zaimis <rd22097@bristol.ac.uk>
2024-11-29 07:10:49 +0000

    Final touches

--- cw_rd22097/regression.py
+++ cw_rd22097/regression.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader, TensorDataset
 
 # Load the training data
 data = pd.read_csv(
-    "/home/stefanos/uni/ml/cw/regression_train.txt", sep=" ", header=None
+    "regression_train.txt", sep=" ", header=None
 )
 
 x_train = data[0].values
@@ -22,8 +22,8 @@ y_train = data[1].values
 # Code Task 10
 lin_reg = LinearRegression()
 lin_reg.fit(x_reshaped, y_train)
-print("Coefficient (slope):", lin_reg.coef_)
-print("Intercept:", lin_reg.intercept_)
+print("Lin Reg Coefficient (slope):", lin_reg.coef_)
+print("Lin REg Intercept:", lin_reg.intercept_)
 
 # Code Task 11
 x_train_tensor = from_numpy(x_reshaped).float()
@@ -65,8 +65,8 @@ for epoch in range(epochs):
         loss.backward()
         optimiser.step()
 
-    if (epoch + 1) % 50 == 0:
-        print(f"Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}")
+    #if (epoch + 1) % 50 == 0:
+        #print(f"Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}")
 
 # Code Task 12
 x_centered = x_train - np.mean(x_train)
@@ -104,7 +104,7 @@ print(az.summary(idata, round_to=2))
 
 # Code Task 13:
 test_data = pd.read_csv(
-    "/home/stefanos/uni/ml/cw/regression_test.txt", sep=" ", header=None
+    "regression_test.txt", sep=" ", header=None
 )
 x_test = test_data[0].values.reshape(-1, 1)
 y_test = test_data[1].values
